b3f363e224224e70adfe88a5c44cef53fa0fdfb0
aaron <aaronhashai@gmail.com>
2022-01-09 20:06:51 +0200

    T WIP still ... still ... compiles now

--- algo.c
+++ algo.c
@@ -144,10 +144,10 @@ int* dijkstra(int **G,int startnode)
 char doTSP() {
 
     int numVerticesInPath = getInt();
-    int *vertexIsInPath = (int*)(malloc(sizeof(int)*numVertices));
+    int *vertexIsInPath = (int*)(malloc(sizeof(int)*numVerticesInPath));
     for (int i = 0; i < numVerticesInPath; i++) {
 	int value = getInt();
-	vertexIsInPath[vertexId] = value;
+	vertexIsInPath[i] = value;
 
     }
     if (DEBUG_EN) printf("%s %d [doTSP] start numVerticesInPath(%d) vertixIsInPath: ", __FILE__, __LINE__, numVerticesInPath);
@@ -173,42 +173,42 @@ void swap(int *a, int *b) {
     *b = tmp;
 }
 
+int** getVerticesPermutations(int numOfPermutations, int numVerticesInPath) {
+    int **ret = allocateTwoDimenArrayOnHeapUsingMalloc(numOfPermutations, numVerticesInPath);
+
+    return ret;
+}
+
 // implementation of traveling Salesman Problem
-int travelingSalesmanProblem(int **graph, Vertices *verticesInPath, int numVerticesInPath)
+int travelingSalesmanProblem(int **graph, int *verticesInPath, int numVerticesInPath)
 {
 
     int numOfPermutations = (int)pow(2, numVerticesInPath);
-    int verticesPermutations_i = 0;
     int **verticesPermutations = getVerticesPermutations(numOfPermutations, numVerticesInPath);
-    bool *verticesPermutationsIsValid = (bool*)(malloc(sizeof(bool)*numOfPermutations));
-
-    for (int i = 0; i < numVerticesInPath; i++) {
-	for (int j = 0; j < numVerticesInPath; j++) {
-	    if (i == j) continue;
-	    verticesPermutations
-	}
-    }
- 
-    // store minimum weight Hamiltonian Cycle.
     int minPath = MAX_WEIGHT;
-    do {
+
+    for (int i = 0; i < numOfPermutations; i++) {
+	int *currentVerticesPermutation = verticesPermutations[i];
+
+    	// store current Path weight(cost)
+    	int currentPathWeight = 0;
  
-        // store current Path weight(cost)
-        int currentPathWeight = 0;
+    	// compute current path weight
+	int fromValue = currentVerticesPermutation[0];
+	int fromGraphId = getVertexId(fromValue);
+    	for (int i = 1; i < numVerticesInPath; i++) {
+	    int toValue = currentVerticesPermutation[i];
+    	    int pathWeight = graph[fromValue][toValue];
+    	    currentPathWeight = (pathWeight == 0) ? MAX_WEIGHT : currentPathWeight + pathWeight;
+    	    fromValue = currentVerticesPermutation[i];
+	    fromGraphId = getVertexId(fromValue);
+    	}
  
-        // compute current path weight
-        int iteratorValue = fromValue;
-        for (int i = 0; i < (numVerticesInPath-1); i++) {
-	    int pathWeight = graph[getVertexId(iteratorValue)][verticesToCheck[i]];
-            currentPathWeight = (pathWeight == 0) ? MAX_WEIGHT : currentPathWeight + pathWeight;
-            iteratorValue = verticesToCheck[i];
-        }
+    	// update minimum
+    	minPath = (minPath < currentPathWeight) ? minPath : currentPathWeight;
  
-        // update minimum
-        minPath = min(minPath, currentPathWeight);
- 
-    } while (
-        next_permutation(vertex.begin(), vertex.end()));
+
+    }
 
     destroyTwoDimenArrayOnHeapUsingFree(verticesPermutations, numVertices,numVertices);
  
--- algo.h
+++ algo.h
@@ -10,7 +10,8 @@ void destroyTwoDimenArrayOnHeapUsingFree(int ** ptr, int row, int col);
 void printSolution(int dist[]);
 char doTSP();
 void TSP(int* C, int* A, int* path, int* fpath, int *sum, int *fsum, int flag, int b, int a, int *sc);
-int travelingSalesmanProblem(int **graph, int fromValue, Vertex *verticesInPath, int numVerticesInPath);
+int travelingSalesmanProblem(int **graph, int *verticesInPath, int numVerticesInPath);
 int** getGraph();
 void print2DGraph(int **graph);
-void swap(int *a, int *b)
+void swap(int *a, int *b);
+int** getVerticesPermutations(int numOfPermutations, int numVerticesInPath);
